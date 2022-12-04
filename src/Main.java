0bc0ede1a02dc285a8a13cb3fab80d6de815b2e0
Eugene <jacky1suzuka@gmail.com>
2022-12-04 17:17:31 +0300

    check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[][] booksAndAuthors = new String[4][2];
+        booksAndAuthors[0][0] = "Война и мир";
+        booksAndAuthors[0][1] = "Лев Николаевич Толстой";
+        booksAndAuthors[1][0] = "Капитанская дочка";
+        booksAndAuthors[1][1] = "Александр Сергеевич Пушкин";
+        booksAndAuthors[2][0] = "Мёртвые души";
+        booksAndAuthors[2][1] = "Николай Васильевич Гоголь";
+        booksAndAuthors[3][0] = "Горе от ума";
+        booksAndAuthors[3][1] = "Александр Сергеевич Грибоедов";
+
+        for (int i = 0; i < booksAndAuthors.length; i++) {
+            for (int j = 0; j < booksAndAuthors[i].length; j++) {
+                if (j == 0) {
+                    System.out.print("«" + booksAndAuthors[i][j] + "»" + " ");
+                }else{
+                    System.out.println(booksAndAuthors[i][j]);
+                }
+            }
+        }
     }
 }
